4344490c1e85935fbd06c6d5f49b3d3f89297465
Jatinder <jatinder_dhawan@yahoo.com>
2016-11-09 19:59:33 +0530

    Added faculty controller+repo

---
+++ src/main/java/org/csspec/IdentityService/api/controller/FacultyController.java
@@ -0,0 +1,29 @@
+package org.csspec.IdentityService.api.controller;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.csspec.IdentityService.api.repo.FacultyRepository;
+import org.csspec.IdentityService.config.RequestValidator;
+import org.csspec.IdentityService.db.Faculty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FacultyController {
+    @Autowired
+    public FacultyRepository facultyRepository;
+
+    @RequestMapping(value = "/identity/faculty", method = RequestMethod.POST)
+    public ResponseEntity<?> addOneFaculty(@RequestBody Faculty faculty, HttpServletRequest request) {
+        RequestValidator.checkHeader(request,"ADMIN","FACULTY");
+        if(faculty.getFacultyId() == null || faculty.getUserID() ==null) {
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+        facultyRepository.save(faculty);
+        return new ResponseEntity<Object>(HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/org/csspec/IdentityService/api/repo/FacultyRepository.java
@@ -0,0 +1,9 @@
+package org.csspec.IdentityService.api.repo;
+
+
+import org.csspec.IdentityService.db.Faculty;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface FacultyRepository extends MongoRepository<Faculty,String>{
+    public Faculty findFacultyByFacultyId(String facultyId);
+}
